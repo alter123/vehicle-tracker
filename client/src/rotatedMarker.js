1c066863adb046b8d0607d7f7361115144d61a6d
alter <jay.vasant@irisindia.net>
2019-11-26 00:33:47 +0530

    code cleanup

--- client/src/rotatedMarker.js
+++ client/src/rotatedMarker.js
@@ -8,36 +8,6 @@ import DynamicPopup from "./dynamicPopup";
 import carIcon from "./static/car.svg";
 
 const RotatedMarker = ({ data }) => {
-  // transitions marker's head from initial angle to final angle
-  // const [currentAngle, changeAngle] = useState(fromAngle);
-
-  // useEffect(() => {
-  //   if (currentAngle === toAngle) return;
-
-  //   // determine the shorter path to reach angle a to angle b
-  //   let rotateClockwise =
-  //     360 - Math.abs(fromAngle - toAngle) > Math.abs(fromAngle - toAngle)
-  //       ? true
-  //       : false;
-
-  //   let increaseAngle = rotateClockwise
-  //     ? fromAngle < toAngle
-  //       ? true
-  //       : false
-  //     : fromAngle < toAngle
-  //     ? false
-  //     : true;
-
-  //   if (currentAngle === 360 || currentAngle === 0)
-  //     changeAngle(increaseAngle ? 0 : 360);
-
-  //   const intervalId = setInterval(() => {
-  //     changeAngle(currentAngle + (increaseAngle ? 1 : -1));
-  //   }, 0);
-
-  //   return () => clearInterval(intervalId);
-  // }, [currentAngle, toAngle, fromAngle]);
-
   const modifiedMarker = ref => {
     // https://stackoverflow.com/a/38971299
     // open popup while loading
